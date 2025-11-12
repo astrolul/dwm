1e3f09a684894cb727be0f2d6f67868ff72d83ba
astrolul <85197489+astrolul@users.noreply.github.com>
2025-11-12 22:51:49 +0000

    ok

--- config.h
+++ config.h
@@ -4,10 +4,13 @@
 
 /* appearance */
 static const unsigned int borderpx  = 5;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int gappx     = GAP_SIZE;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+#ifndef GAP_SIZE
+#define GAP_SIZE 5
+#endif
 #ifndef ICONSIZE
 #define ICONSIZE 32   /* icon size */
 #endif
