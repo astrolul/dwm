7cb9f8301b6ee4a6b1654594211d61ad015d98dd
astrolul <85197489+astrolul@users.noreply.github.com>
2025-11-12 16:49:00 +0000

    Update font size for FiraCode Nerd Font Mono

--- config.def.h
+++ config.def.h
@@ -13,7 +13,7 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 48;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[]          = { 
         "Fira Code:style=Regular:size=14",
-        "FiraCode Nerd Font Mono:style=Regular:size=20",
+        "FiraCode Nerd Font Mono:style=Regular:size=22",
 };
 static const char dmenufont[]       = "monospace:size=10";
 static const char *brupcmd[] 		= { "brightnessctl", "set", "10%+", NULL };
